f3d212654afced9a3ee50034b60a24e8c00aebed
Sanket Chirame <sanketchirame12@gmail.com>
2018-05-12 16:33:47 +0530

    changed test case

--- test_frames.py
+++ test_frames.py
@@ -3,6 +3,9 @@ import unittest	#testing library
 import numpy as np
 from ddt import ddt,file_data,unpack,data
 from math import sin,cos
+def add(a,b,c):
+	return a+b+c
+
 @ddt
 class TestLatLon(unittest.TestCase):
 	@file_data("test-data/test_latlon.json")
@@ -12,14 +15,14 @@ class TestLatLon(unittest.TestCase):
 		result = fr.latlon(given)
 		self.assertTrue(np.allclose([result[0],result[1]],expected))	
 		self.assertEqual(type(result),tuple)
-	@data(1,2,3,4,5,6)
+	@data(1,2,3,4,5,6,7,8)
 	def test_z_symmetry_lat(self,value):
 		#self.assertEqual(2,2)
 		x1 = np.array([sin(value),cos(value),13.])
 		self.assertAlmostEqual(fr.latlon(x1)[0],fr.latlon(np.array([1.,0.,13.]))[0])
 	@data(7e3,7.1e3,7.2e3,7.3e3,7.4e3)
 	def test_independnt_of_radius(self,radius):
-		x1 = np.array([1.,2.,-3.])
+		x1 = np.array([1.0,2.0,-3.0])
 		self.assertAlmostEqual(fr.latlon(radius*x1)[0],fr.latlon(x1)[0])
 		self.assertAlmostEqual(fr.latlon(radius*x1)[1],fr.latlon(x1)[1])
 
